1bd712e1041fd4c3220019972336f8fa35659f91
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-12-09 23:40:37 -0500

    feat(app): add config initial

---
+++ src/components/common/index.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 1rem;
+`;
+
+export const Button = styled.button`
+  cursor: pointer;
+  text-align: center;
+  font-weight: bold;
+  background-color: ${({ theme }) => theme.color.button};
+  border: 1px solid ${({ theme }) => theme.color.border};
+  color: ${({ theme }) => theme.color.textPrincipal};
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  &:active {
+    transform: scale(0.97);
+  }
+`;
+
+//layout APP
+export const Layout = styled.div`
+  padding: 1rem;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    gap: 1rem;
+  }
+`;
+
+export const Main = styled.main`
+  flex: 3;
+`;
+
+export const Aside = styled.aside`
+  flex: 1;
+`;
