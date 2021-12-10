19b6a7a58f14d07608f88cf505fc068445017012
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-12-09 23:40:20 -0500

    feat(app): add TabNavigation component

---
+++ src/components/TabNavigation.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { FiBookOpen, FiBook, FiBox, FiBriefcase } from 'react-icons/fi';
+
+const TabNavigation = () => {
+  return (
+    <StyledTabNavigation>
+      <TabItem>
+        <FiBookOpen />
+        Overview
+      </TabItem>
+      <TabItem active={true}>
+        <FiBook />
+        Repositories <TotalRepositories>45</TotalRepositories>
+      </TabItem>
+      <TabItem>
+        <FiBriefcase />
+        Project
+      </TabItem>
+      <TabItem>
+        <FiBox />
+        Packages
+      </TabItem>
+    </StyledTabNavigation>
+  );
+};
+
+const StyledTabNavigation = styled.ul`
+  /* padding-top: 2rem 0 1rem; */
+  display: flex;
+  gap: 2rem;
+  overflow: auto;
+  border-bottom: 1px solid ${({ theme }) => theme.color.border};
+  background-color: ${({ theme }) => theme.color.primary};
+  position: sticky;
+  top: 0;
+`;
+
+const TabItem = styled.li<{ active?: Boolean }>`
+  display: flex;
+  gap: 0.2rem;
+  color: ${({ theme }) => theme.color.textPrincipal};
+  padding: 2rem 0 1rem;
+  border-bottom: ${({ theme, active }) =>
+    active ? `1px solid ${theme.color.borderHeader}` : 'red'};
+`;
+
+const TotalRepositories = styled.strong`
+  background: ${({ theme }) => theme.color.button};
+  padding: 0 0.5rem;
+  border-radius: 2rem;
+`;
+export default TabNavigation;
