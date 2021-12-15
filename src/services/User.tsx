5999b6fc06ef5376b69256e894228be4dd130f63
davidmariolc <davidmariolc.dev@gmail.com>
2021-12-14 22:29:55 -0500

    feat(app): add services github

--- src/services/User.tsx
+++ src/services/User.tsx
@@ -1,1 +1,13 @@
-export const fetchGithubApi = () => {};
+export const fetchGithubApi = async (username: string): Promise<any> => {
+  const API_URL: string = 'https://api.github.com/users';
+  const [userResponse, repositoriesResponse] = await Promise.all([
+    fetch(`${API_URL}/${username}`),
+    fetch(`${API_URL}/${username}/repos`),
+  ]);
+
+  const userExist = userResponse.ok;
+
+  const user = await userResponse.json();
+  const repositories = await repositoriesResponse.json();
+  return { userExist, user, repositories };
+};
