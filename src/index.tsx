717c5a5e3d894dbbb0f98b9bb52ab27424a72f91
davidmariolc <davidmariolc.dev@gmail.com>
2021-12-18 16:52:04 -0500

    feat(app):  add routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,23 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-// import NotFound from './components/NotFound';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path=':username' element={<App />} />
         <Route
-          path='*'
-          element={<Navigate to='/davidmariolc' replace={true} />}
+          path='/'
+          element={<Navigate to='/kentcdodds' replace={true} />}
         />
+        <Route path=':username' element={<App />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
